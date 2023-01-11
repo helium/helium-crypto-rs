5d2fd0fb8bd35105a7f385e5b983d8004bdc880d
Matthew Plant <maplant95@gmail.com>
2023-01-10 20:01:04 -0500

    Downgrade crypto libraries to support the latest version of solana client  (#46)
    * Downgrade crypto libraries to support the latest version of solana
    
    * Fix clippy errors for base64

--- src/ecc_compact/mod.rs
+++ src/ecc_compact/mod.rs
@@ -33,7 +33,7 @@ pub trait IsCompactable {
 
 impl IsCompactable for p256::PublicKey {
     fn is_compactable(&self) -> bool {
-        self.as_affine().to_compact_encoded_point().is_some().into()
+        self.as_affine().to_compact_encoded_point().is_some()
     }
 }
 
@@ -216,10 +216,11 @@ impl TryFrom<&[u8]> for PublicKey {
                 p256::EncodedPoint::from_bytes(input).map_err(p256::elliptic_curve::Error::from)?;
             // Convert to an affine point, then to the compact encoded form.
             // Then finally convert to the p256 public key.
-            let public_key: Option<_> = p256::AffinePoint::from_encoded_point(&encoded_point)
+            let public_key = Option::from(p256::AffinePoint::from_encoded_point(&encoded_point))
                 .and_then(|affine_point: p256::AffinePoint| affine_point.to_compact_encoded_point())
-                .and_then(|compact_point| p256::PublicKey::from_encoded_point(&compact_point))
-                .into();
+                .and_then(|compact_point| {
+                    Option::from(p256::PublicKey::from_encoded_point(&compact_point))
+                });
             Ok(PublicKey(public_key.ok_or_else(Error::not_compact)?))
         }
     }
@@ -343,9 +344,6 @@ mod tests {
         // And now do an ecdh with my keypair and the other public key and
         // compare it with the shared secret that the erlang ecdh generated
         let shared_secret = keypair.ecdh(&other_public_key).expect("shared secret");
-        assert_eq!(
-            shared_secret.raw_secret_bytes().as_slice(),
-            OTHER_SHARED_SECRET
-        );
+        assert_eq!(shared_secret.as_bytes().as_slice(), OTHER_SHARED_SECRET);
     }
 }
